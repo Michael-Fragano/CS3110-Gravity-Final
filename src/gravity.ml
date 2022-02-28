d33360b6a75396531884079eb164446be3fcc6e7
Michael Fragano <mjf347@cornell.edu>
2022-02-28 16:23:37 -0500

    more work on from_json

--- src/gravity.ml
+++ src/gravity.ml
@@ -2,7 +2,7 @@ open Yojson.Basic.Util
 
 type position = {
   x : float;
-  y: float;
+  y : float;
 }
 
 type velocity = {
@@ -17,22 +17,28 @@ type body = {
 }
 
 type system = {
-  g: float;
-  bodies: body list;
+  g : float;
+  bodies : body list;
 }
 
+(*helper functions for from_json*)
+let pos_json p : position = { x = hd p; y = tl p }
+let vel_json v : velocity = { x = hd v; y = tl v }
 
+let body_json j =
+  {
+    pos = pos_json (member "position" j);
+    vel = vel_json (member "velocity" j);
+    mass = to_float (member "mass" j);
+  }
 
 let from_json json =
- {
-   
- }
-  
-let move b =
-  raise (Failure "Unimplemented: Gravity.move")
+  {
+    g = to_float (member "g" json);
+    bodies = List.map body_json (to_list (member "bodies" json));
+  }
 
+let move b = raise (Failure "Unimplemented: Gravity.move")
 
-
-let rec frame s : system = 
+let rec frame s : system =
   raise (Failure "Unimplemented: Gravity.frame")
-
