9629cd6ff83d41a3d719b09cdb8001a39f9ed1cd
Seth Norman <snorman7384@gmail.com>
2022-02-28 17:22:34 -0500

    Add specs: Gravity.grav_field and Gravity.dist_sq

--- src/gravity.mli
+++ src/gravity.mli
@@ -16,7 +16,15 @@ val from_json : Yojson.Basic.t -> system
 (** [from_json s] is the system that [s] represents. Requires: [s] is a
     valid JSON system representation*)
 
-val move : body -> body
+val dist_sq : body -> body -> float
+(** [dist_sq a b] is the square of the distance between two bodies [a]
+    and [b]*)
+
+val grav_field : system -> body -> float
+(** [grav_field s b] is the total gravitational field experienced by [b]
+    in [s] from the other bodies in [s]*)
+
+val move : system -> body -> body
 (** [move b] takes a body and determines what its new position is after
     one frame*)
 
