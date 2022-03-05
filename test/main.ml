ff2adaf851abb71b942a11c6dfe4c411673efd89
Michael Fragano <mjf347@cornell.edu>
2022-03-04 21:21:06 -0500

    Fixed up grav_field more, one test failing idk why

--- test/main.ml
+++ test/main.ml
@@ -4,16 +4,20 @@ open Gravity
 let tst = Yojson.Basic.from_file "data/test_system.json"
 let tsys = from_json tst
 let b = bods tsys
+let x = gx (grav_field tsys (bodies_ex b (List.hd b)) (List.hd b))
+let y = gy (grav_field tsys (bodies_ex b (List.hd b)) (List.hd b))
 
 let gravity_tests =
+  print_float x;
+  print_float y;
   [
     ( "Gravitational Constant" >:: fun t ->
       assert_equal 0.05 (g_const tsys) );
     ( "Gravitational Field x component" >:: fun t ->
-      assert_equal 0.15625
+      assert_equal 0.08
         (gx (grav_field tsys (bodies_ex b (List.hd b)) (List.hd b))) );
     ( "Gravitational Field y component" >:: fun t ->
-      assert_equal 0.0
+      assert_equal ~-.0.06
         (gy (grav_field tsys (bodies_ex b (List.hd b)) (List.hd b))) );
   ]
 
