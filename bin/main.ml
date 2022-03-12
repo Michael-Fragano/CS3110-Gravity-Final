99ac84c57e8082ae1f83ef288f72d1accb42c11f
Michael Fragano <mjf347@cornell.edu>
2022-03-12 18:12:14 -0500

    Added text prompts and a way to quit

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,13 @@
-try Visuals.start_window_preset "binary"
-with Graphics.Graphic_failure "fatal I/O error" ->
-  print_endline "Thanks for using the gravity simulator!"
+let rec run_sim () =
+  print_endline
+    "What system do you want to simulate? Or type 'Q' to quit.";
+  let file = read_line () in
+  if file = "Q" then
+    print_endline "Thanks for using the gravity simulator!"
+  else
+    try Visuals.start_window_preset file
+    with Graphics.Graphic_failure "fatal I/O error" ->
+      Graphics.close_graph ();
+      run_sim ()
+in
+run_sim ()
