fa3ec4dfa628aeb8a8b803e58a53f3b0b3535cab
Michael Fragano <mjf347@cornell.edu>
2022-04-26 16:25:04 -0400

    commented out my unfinished code

--- src/visuals/create.ml
+++ src/visuals/create.ml
@@ -3,9 +3,7 @@ type create_state =
   | Size
   | Velocity
 
-let poll (status : Status.t) (system : Gravity.system) : Status.t =
-  ( status |> Status.poll_input |> Status.update_body_num system
-  |> fun s ->
-    if s.mouse_state = Pressed then Status.toggle_pause s else s )
-  |> fun s ->
-  if s.space_state = Pressed then Status.cycle_focus s else s
+(**let poll (status : Status.t) (system : Gravity.system) : Status.t = (
+   status |> Status.poll_input |> Status.update_body_num system |> fun s
+   \-> if s.mouse_state = Pressed then Status.toggle_pause s else s ) |>
+   fun s -> if s.space_state = Pressed then Status.cycle_focus s else s*)
